182c1d7bcea3535a37c2edac849c8b5317b95334
stevenzg <guoyinfeng@live.com>
2023-04-09 21:30:21 +0800

    feat: init PageHeader

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, useEffect, useState } from 'react'
+import { useApplet } from '../Provider'
+
+interface PageHeaderProps {
+  headerTitle: string | undefined,
+  headerRightActions: ReactNode | undefined
+}
+
+export function PageHeader({ headerTitle, headerRightActions }: PageHeaderProps) {
+  const applet = useApplet()
+  const [hasConfigNavigation, setHasConfigNavigation] = useState(false)
+
+  useEffect(() => {
+    if (applet && !hasConfigNavigation) {
+      applet.setHeaderTitle(headerTitle)
+      applet.setHeaderRightActions(headerRightActions)
+      setHasConfigNavigation(true)
+    }
+  }, [applet])
+
+  return (
+    <></>
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './layouts/DefaultLayout'
 export * from './layouts/SubPageLayout'
 
 export * from './Provider'
+
+export * from './components/PageHeader'
