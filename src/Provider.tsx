6215a5b1a82101af94a6717f0cb6cfdb448d8d3a
stevenzg <guoyinfeng@live.com>
2023-04-13 22:30:04 +0800

    feat: support config theme

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -5,6 +5,8 @@ import toast, { Toaster } from 'react-hot-toast'
 
 export interface AppletContxtType {
   profile: User | undefined
+  theme: string | null
+  configTheme: (themeMode: string) => void
   logOut: () => void
   headerTitle: string | undefined
   setHeaderTitle: (title: string | undefined) => void
@@ -21,6 +23,7 @@ export function AppletProvider(props: { children: React.ReactNode }) {
   const [headerRightActions, setHeaderRightActions] = useState<ReactNode | undefined>(undefined)
   const [headerTitle, setHeaderTitle] = useState<string | undefined>(undefined)
   const [profile, setProfile] = useState<User | undefined>(undefined)
+  const [theme, setTheme] = useState<string | null>(null)
 
   useEffect(() => {
     const initUser = async () => {
@@ -33,6 +36,26 @@ export function AppletProvider(props: { children: React.ReactNode }) {
     initUser()
   }, [])
 
+  useEffect(() => {
+    const localTheme = localStorage.getItem('theme')
+    setTheme(localTheme)
+    if (localTheme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
+
+  const configTheme = (newThemeMode: string) => {
+    setTheme(newThemeMode)
+    localStorage.setItem('theme', newThemeMode)
+    if (newThemeMode === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  };
+
   const logOut = () => {
     localStorage.removeItem('TOKEN')
     setProfile(undefined)
@@ -43,6 +66,8 @@ export function AppletProvider(props: { children: React.ReactNode }) {
       value={{
         profile,
         logOut,
+        theme,
+        configTheme,
         headerTitle,
         setHeaderTitle,
         headerRightActions,
--- src/components/MobileNavigation.tsx
+++ src/components/MobileNavigation.tsx
@@ -5,6 +5,7 @@ import { useApplet } from '../Provider'
 export default function MobileNavigation ({ onSelectMenu, menus }: { onSelectMenu: () => void, menus: Menu[] }) {
   const applet = useApplet()
   const userProfile = applet?.profile
+  const theme = applet?.theme
   const navigate = useNavigate()
 
   const logOut = () => {
@@ -13,20 +14,20 @@ export default function MobileNavigation ({ onSelectMenu, menus }: { onSelectMen
   }
 
   return (
-    <div className="md:hidden flex flex-col min-h-screen pt-4 bg-white">
+    <div className="md:hidden flex flex-col min-h-screen pt-4 bg-white dark:bg-gray-900">
       <div className="flex-1">
         <div className="flex items-center justify-center mb-4">
           <img
             className="w-12 h-12 rounded-full cursor-pointer"
             src={`${userProfile?.avatarUrl}!avatar`}
             alt="Avatar" />
-          <div className="ml-4 text-lg font-semibold">{userProfile?.name}</div>
+          <div className="ml-4 text-lg font-semibold text-gray-900 dark:text-gray-100">{userProfile?.name}</div>
         </div>
         {menus.map((item) => (
           <Link
             key={item.path}
             to={item.path}
-            className="block py-2 px-4 hover:bg-gray-700 transition-colors"
+            className="block py-2 px-4 hover:bg-gray-700 dark:hover:bg-gray-600 transition-colors text-gray-900 dark:text-gray-100"
             onClick={onSelectMenu}
           >
             {item.label}
@@ -34,10 +35,16 @@ export default function MobileNavigation ({ onSelectMenu, menus }: { onSelectMen
         ))}
       </div>
 
-      <div className="flex-shrink-0 mb-4">
+      <div className="flex-shrink-0 mb-4 mx-5">
+        <button
+          onClick={() => applet?.configTheme(theme === 'dark' ? 'light' : 'dark')}
+          className="w-full mb-2 bg-gray-800 text-white py-2 px-4 rounded-md shadow-md dark:bg-white dark:text-gray-800"
+        >
+          { theme === 'dark' ? 'Light Mode' : 'Dark Mode' }
+        </button>
         <button
           onClick={logOut}
-          className="w-full py-2 px-4 font-semibold transition-colors"
+          className="w-full py-2 px-4 font-semibold bg-red-500 text-white rounded-md hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700 transition-colors"
         >
           Log out
         </button>
